b0d84969ccad5e107b1067be13874e6d189c8bd4
Matthew Seagren <mattseagren@gmail.com>
2021-12-02 20:19:17 -0600

    Created a database file to connect to mongos. Connected it to the server, tested a get and post request and apperently mongoose failed to connect the mongodb

---
+++ server/database/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// Connect to the database
+mongoose.connect('mongodb://localhost/mvp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Create a Schema to store information
+const accountSchema = new mongoose.Schema({
+  userName: String,
+  password: String,
+  email: String,
+  displayName: String
+});
+
+// Compile the schema into a model
+const Account = mongoose.model('Account', accountSchema);
+
+// Use this to save new data points to the mongo database
+let save = (data) => {
+  var user = data.userName;
+  var pass = data.password;
+  var email = data.email;
+  var display = data.displayName;
+  const account = new Account({ userName: user, password: pass, email: email, displayName: display });
+  account.save();
+}
+
+// Use this when getting all info from the database
+let get = (callback) => {
+  console.log('Starting get request');
+  const account = Account.find({}).exec((err, data) => {
+    if (err) {
+      console.log('ERROR: could not Get data from database');
+    } else {
+      console.log('Get request finished');
+      callback(data);
+    }
+  });
+}
+
+module.exports.save = save;
+module.exports.get = get;
--- server/index.js
+++ server/index.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require('./database');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Get Request Recieved')
+})
+
+app.get('/accounts', (req, res) => {
+  console.log('GET request recieved');
+  db.get((data) => {
+    res.send(data);
+  });
+});
+
+app.post('/accounts', (req, res) => {
+  console.log('POST request recieved');
+  db.save(req.body);
+  res.send('Post Request Recieved');
 })
 
 app.listen(port, () => {
