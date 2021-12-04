4d10c2d228be91a981c2bf544834c9e6d7b9666d
Matthew Seagren <mattseagren@gmail.com>
2021-12-03 19:50:27 -0600

    Started working on getting basing info and forming the react app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,1 +1,30 @@
 // This is the primary component of the react app
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // this.binds go here
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="title">Hygieia</h1>
+        <nav className="nav">Nav Bar</nav>
+        <p>What habit are you working on?</p>
+        <button type='submit'>Add Habit</button>
+        <div>
+          <h4>Working Out</h4>
+          <button type="button">Did you perform this habit Today?</button>
+          <p>Habit Score: 609</p>
+          <p>Streak: 64</p>
+          <button type="button">View Entries</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,9 @@
 // This is the start of the react app
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <App />,
   document.getElementById('root')
 );
